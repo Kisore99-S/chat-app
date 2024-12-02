1d92b13b4c06f593c8e3e44184f9f70952daa1b3
Kisore <kisore.s70@gmail.com>
2024-12-02 17:45:27 +0530

    add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <div className="h-screen bg-gray-50 dark:bg-gray-900">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 }
