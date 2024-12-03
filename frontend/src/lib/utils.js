c8556bbdd3df75f9e6e076347c4d5eca664f9c4c
Kisore <kisore.s70@gmail.com>
2024-12-03 16:53:46 +0530

    feat: add group messages by date

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,29 @@
-export function formatMessageTime(date) {
+import { isToday, isYesterday, format } from "date-fns";
+
+export const formatMessageTime = (date) => {
   return new Date(date).toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
-    hour12: false,
+    hour12: true,
   });
-}
+};
+
+export const formatDateLabel = (date) => {
+  const messageDate = new Date(date);
+
+  if (isToday(date)) return "Today";
+  if (isYesterday(date)) return "Yesterday";
+
+  return format(messageDate, "MMMM dd yyyy");
+};
+
+export const groupMessagesByDate = (messages) => {
+  return messages.reduce((acc, message) => {
+    const dateKey = format(new Date(message.createdAt), "yyyy-MM-dd");
+    console.log("date key", dateKey);
+    if (!acc[dateKey]) acc[dateKey] = [];
+    acc[dateKey].push(message);
+
+    return acc;
+  }, {});
+};
